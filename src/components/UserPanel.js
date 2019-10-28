388265a928ab9e7a069db186b990d4425f4947f7
Jeremy Josey <jjosey@getchalk.com>
2019-10-27 21:39:40 -0300

    Added watchlist usersettings buttons

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import Button from './Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilm, faUserCog } from '@fortawesome/free-solid-svg-icons';
 
 export default function UserPanel() {
-	return <div className="userpanel">userpanel here</div>;
+	return (
+		<div className="user-panel">
+			<Button cssname="btn-film">
+				<FontAwesomeIcon icon={faFilm} className="film-icon" />
+			</Button>
+			<Button cssname="btn-cog">
+				<FontAwesomeIcon icon={faUserCog} className="cog-icon" />
+			</Button>
+		</div>
+	);
 }
