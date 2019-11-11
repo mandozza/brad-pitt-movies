7145d47addcda3cfd7f0f1c32615a87541eb4812
Jeremy Josey <jjosey@getchalk.com>
2019-11-10 21:00:10 -0400

    Moved movie car to a styled component

---
+++ src/components/MovieCard/index.js
@@ -0,0 +1,57 @@
+import React, { useContext } from 'react'
+import { UserContext } from '../UserContext'
+import styled from 'styled-components'
+import Button from '../Buttons'
+
+const MovieCardPanel = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: #ccc;
+  border-radius: 10px;
+  box-shadow: 6px 6px 5px -4px rgba(0, 0, 0, 0.28);
+  margin: 10px;
+  padding: 10px 10px;
+  width: 180px;
+`
+
+const MovieCardTitle = styled.h3`
+  word-wrap: normal;
+  font-size: 16px;
+`
+
+const MovieCardImage = styled.img`
+  max-width: 200px;
+  max-height: 200px;
+`
+
+const MovieCard = ({ name, image, id, movieStatus }) => {
+  const { userMovieList, setUserMovieList } = useContext(UserContext)
+  const addToWatchList = e => {
+    console.log('here')
+    setUserMovieList([
+      ...userMovieList,
+      {
+        name: name,
+        image: image,
+        id: id,
+        movieStatus: 'watch',
+      },
+    ])
+  }
+
+  return (
+    <MovieCardPanel key={id} className="{{statusStyle}}">
+      <MovieCardImage src={image} alt={name} />
+      <MovieCardTitle>{name}</MovieCardTitle>
+      <Button type="btnadd" onClick={() => addToWatchList()}>
+        {movieStatus === 'watch'
+          ? 'Remove from Watchlist'
+          : 'Add to Watch List'}
+      </Button>
+    </MovieCardPanel>
+  )
+}
+
+export default MovieCard
